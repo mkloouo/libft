89ea96fac9e9d237fcc4307923806e17c31a7b76
Mykola ODNOSUMOV <modnosum@e2r1p6.unit.ua>
2018-01-04 20:02:03 +0200

    Remove unnecessary import

--- includes/libft.h
+++ includes/libft.h
@@ -6,17 +6,12 @@
 /*   By: modnosum <modnosum@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/02 19:19:07 by modnosum          #+#    #+#             */
-/*   Updated: 2018/01/04 19:39:51 by modnosum         ###   ########.fr       */
+/*   Updated: 2018/01/04 20:01:30 by modnosum         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
-
-/*
-** string.h is for size_t import.
-*/
-# include <string.h>
 
 # include "ft_list.h"
 # include "ft_io.h"
