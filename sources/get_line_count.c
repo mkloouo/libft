12a01cb517194ec572c034b3280739f5d8ca53d6
Mykola ODNOSUMOV <modnosum@e1r2p11.unit.ua>
2018-02-25 19:46:32 +0200

    Fix line counter

--- sources/get_line_count.c
+++ sources/get_line_count.c
@@ -6,7 +6,7 @@
 /*   By: modnosum <modnosum@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/22 00:09:35 by modnosum          #+#    #+#             */
-/*   Updated: 2018/02/22 04:23:42 by modnosum         ###   ########.fr       */
+/*   Updated: 2018/02/25 18:25:40 by modnosum         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,29 @@ int						get_line_count(const char *file)
 	char				buff[BUFF_SIZE + 1];
 	int					fd;
 	int					count;
+	ssize_t				p;
 	ssize_t				r;
 
+	p = 0;
+	r = 0;
 	count = 0;
 	if ((fd = open_file(file, FILE_READ)) != -1)
 	{
 		while ((r = read(fd, buff, BUFF_SIZE)) > 0)
 		{
+			p = r;
 			buff[r] = 0;
 			count += ft_strcc(buff, '\n');
 		}
+		if (p != r && buff[p - 1] != '\n')
+			count++;
 		close_file(fd);
 	}
 	return (count);
 }
+/*
+** 'asdasd\newef\nasdas\n'
+** '\n'
+**
+**
+*/
