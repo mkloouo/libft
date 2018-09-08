1e82822ed696ccd4c17295fcde9f361054fb062a
Mykola Odnosumov <modnosum@gmail.com>
2018-09-08 20:10:19 +0300

    Remove

--- source/conv/ft_strtoi.c
+++ source/conv/ft_strtoi.c
@@ -6,20 +6,18 @@
 /*   By: modnosum <modnosum@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/08 15:14:10 by modnosum          #+#    #+#             */
-/*   Updated: 2018/09/08 15:26:54 by modnosum         ###   ########.fr       */
+/*   Updated: 2018/09/08 20:09:20 by modnosum         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft/conv.h>
 
-#include <limits.h>
 #include <ft/char.h>
 
-int			ft_strtoi(char const *str, char *err)
+int			ft_strtoi(char const *str)
 {
 	long	result;
 
 	result = ft_strtoli(str);
-	*err = (result > INT_MAX || result < INT_MIN);
 	return ((int)result);
 }
