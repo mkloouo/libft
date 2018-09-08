311fe7b7684019f7d981e18d0f575474eb55a4fc
Mykola Odnosumov <modnosum@gmail.com>
2018-09-08 20:10:19 +0300

    Remove

--- include/ft/conv.h
+++ include/ft/conv.h
@@ -6,7 +6,7 @@
 /*   By: modnosum <modnosum@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/02 20:16:19 by modnosum          #+#    #+#             */
-/*   Updated: 2018/09/08 15:26:51 by modnosum         ###   ########.fr       */
+/*   Updated: 2018/09/08 20:09:26 by modnosum         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char					*ft_itoa_base(int value, int base);
 int						ft_atoi(const char *str);
 double					ft_atof(const char *str);
 
-int						ft_strtoi(char const *str, char *err);
+int						ft_strtoi(char const *str);
 long					ft_strtoli(char const *str);
 
 #endif
