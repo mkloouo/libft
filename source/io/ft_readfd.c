bdb8f8b88c8bc6a60268a4c2ce8f85146b56c828
Mykola Odnosumov <modnosum@gmail.com>
2018-10-18 02:17:47 +0300

    Update cmake support

--- source/io/ft_readfd.c
+++ source/io/ft_readfd.c
@@ -3,23 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   ft_readfd.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: modnosum <modnosum@student.42.fr>          +#+  +:+       +#+        */
+/*   By: modnosum <modnosum@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/16 21:50:33 by modnosum          #+#    #+#             */
-/*   Updated: 2018/10/17 21:38:22 by modnosum         ###   ########.fr       */
+/*   Updated: 2018/10/18 01:11:16 by modnosum         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft/io.h>
 
 #include <ft/string.h>
+#include <ft/memory.h>
 
 char		*ft_readfd(int fd)
 {
 	char	*total;
 	char	buff[BUFF_SIZE + 1];
-	size_t	t_size;
-	size_t	t_cur;
+	ssize_t	t_size;
+	ssize_t	t_cur;
 	ssize_t	rd;
 
 	total = 0;
@@ -30,10 +31,11 @@ char		*ft_readfd(int fd)
 		buff[rd] = 0;
 		if (t_cur + rd > t_size)
 		{
-			total = ft_resize_str(total, t_size, (t_size * 2) + rd);
+			total = ft_resize_str(total, (size_t)t_size,
+					(size_t)(t_size * 2) + rd);
 			t_size = (t_size * 2) + rd;
 		}
-		ft_strncpy(&total[t_cur], buff, (size_t)rd);
+		ft_memcpy(&total[t_cur], buff, (size_t)rd);
 		t_cur += rd;
 	}
 	if (rd == 0 && total == 0)
