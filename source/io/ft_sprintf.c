e36905650f4173085d1a6bc82d7d2564730e8b24
Mykola Odnosumov <modnosum@gmail.com>
2018-10-18 05:25:34 +0300

    Make pass by norm

--- source/io/ft_sprintf.c
+++ source/io/ft_sprintf.c
@@ -6,7 +6,7 @@
 /*   By: modnosum <modnosum@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/07 17:48:36 by modnosum          #+#    #+#             */
-/*   Updated: 2018/10/14 23:06:08 by modnosum         ###   ########.fr       */
+/*   Updated: 2018/10/18 04:24:02 by modnosum         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@
 
 int			ft_sprintf(char *dst, char const *fmt, ...)
 {
-	char 	*str;
+	char	*str;
 	int		ret;
 	va_list	args;
 
 	va_start(args, fmt);
 	ret = ft_vsprintf(&str, fmt, &args);
 	if (ret > 0)
-		ft_strncpy(dst, str, (size_t) ret);
+		ft_strncpy(dst, str, (size_t)ret);
 	free(str);
 	va_end(args);
 	return (ret);
